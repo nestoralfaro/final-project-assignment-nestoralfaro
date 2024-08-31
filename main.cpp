1e751403d564cd9edf61e0795c924723853ad75e
Nestor Alfaro <nalfarocs@gmail.com>
2024-08-30 21:49:35 -0500

    improved alert rate and detection algorithm

--- main.cpp
+++ main.cpp
@@ -1,22 +1,32 @@
+#include <cstdint>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+#define MANY_SPOTS 3
+#define OPEN 0
+#define BUSY 1
+#define CREATE_SPOTS_ARRAY(type, name) type name[MANY_SPOTS]
+
 using namespace cv;
 using namespace std;
+typedef unsigned short ushort;
 
 int main(int argc, char** argv) {
   // load the video
-  string videoPath = "../tmp/parking/v4.mp4";
+  string videoPath = "../tmp/parking/vfast1.mp4";
   VideoCapture cap(videoPath);
+  // VideoCapture cap(0);
   if (!cap.isOpened()) {
     cerr << "Failed to open video" << endl;
     return -1;
   }
 
+  uint16_t bitPosition;
   Mat frame, gray, blurred, edges;
+  CREATE_SPOTS_ARRAY(uint16_t, spots);
+  CREATE_SPOTS_ARRAY(bool, flags);
   while (true) {
     cap >> frame;
-
     if (frame.empty()) {
       cout << "End of video, looping..." << endl;
       cap.set(CAP_PROP_POS_FRAMES, 0);
@@ -25,21 +35,16 @@ int main(int argc, char** argv) {
 
     vector<Point> totalPoints;
 
-    // convert to grayscale
     cvtColor(frame, gray, COLOR_BGR2GRAY);
-
-    // apply Gaussian Blur
     GaussianBlur(gray, blurred, Size(5, 5), 1.5);
-
     // canny Edge Detection
     Canny(blurred, edges, 50, 150);
-
     // detect lines using Hough Transform
     vector<Vec4i> lines;
     HoughLinesP(edges, lines, 1, CV_PI/180, 50, 50, 10);
 
     // draw detected lines
-    for (size_t i = 0; i < lines.size(); i++) {
+    for (ushort i = 0; i < lines.size(); i++) {
       Vec4i l = lines[i];
       Point p1(l[0], l[1]), p2(l[2], l[3]);
       totalPoints.push_back(p1);
@@ -53,60 +58,75 @@ int main(int argc, char** argv) {
     findContours(edges, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
     // filter contours and draw bounding boxes
-    for (size_t i = 0; i < contours.size(); i++) {
+    for (ushort i = 0; i < contours.size(); i++) {
       Rect boundRect = boundingRect(contours[i]);
-      // adjust the area threshold as needed
-      if (boundRect.area() > 500) {
-        totalPoints.push_back(boundRect.tl());
-        totalPoints.push_back(boundRect.br());
-        rectangle(frame, boundRect.tl(), boundRect.br(), Scalar(0, 0, 255), 2);
-
-        // check if the bounding box is between two lines
-        for (size_t j = 0; j < lines.size(); j++) {
-          Vec4i l = lines[j];
-          Point linePoint1(l[0], l[1]);
-          Point linePoint2(l[2], l[3]);
-          // Assuming horizontal lines
-          if (linePoint1.y == linePoint2.y) {
-            if (boundRect.y >= min(linePoint1.y, linePoint2.y) &&
-              boundRect.y + boundRect.height <= max(linePoint1.y, linePoint2.y)) {
-              putText(frame, "Car Detected", boundRect.tl(), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 2);
-              break;
-            }
-          }
-        }
-      }
+      totalPoints.push_back(boundRect.tl());
+      totalPoints.push_back(boundRect.br());
+      // rectangle(frame, boundRect.tl(), boundRect.br(), Scalar(0, 0, 255), 2);
     }
 
-    int rectWidth = frame.cols / 3;
-    int rectHeight = frame.rows;
-    for (int i = 0; i < 3; ++i) {
-      Point topLeft(i * rectWidth, 0);
-      Point bottomRight((i + 1) * rectWidth, rectHeight);
-
+    int spotBoundaryWidth = frame.cols / MANY_SPOTS;
+    int spotBoundaryHeight = frame.rows;
+    for (ushort i = 0; i < MANY_SPOTS; ++i) {
+      Point topLeft(i * spotBoundaryWidth, 0);
+      Point bottomRight((i + 1) * spotBoundaryWidth, spotBoundaryHeight);
+      // boundary rectangles
       rectangle(frame, topLeft, bottomRight, Scalar(255, 0, 0), 2);
 
       // display the count on the frame
-      int count = 0;
-      for (size_t i = 0; i < totalPoints.size(); i++) {
+      ushort count = 0;
+      for (ushort i = 0; i < totalPoints.size(); i++) {
         if (totalPoints[i].x >= topLeft.x && totalPoints[i].x <= bottomRight.x && totalPoints[i].y >= topLeft.y && totalPoints[i].y <= bottomRight.y) {
           count += 1;
         }
       }
-      string text = to_string(count) + (count > 25 ? "Car!" : "");
+      if (count > (spotBoundaryHeight * 0.09)) {
+        // turn the next bit on in a ring-like fashion
+        spots[i] |= (1 << bitPosition);
+      }
+      else {
+        // turn the next bit off in a ring-like fashion
+        // spots[i] &= ~(1 << bitPosition);
+
+        // just reset the bits instead
+        // thus making the real-time car detection more demanding
+        // i.e., all the bits must be turned on in a streak
+        spots[i] = 0;
+      }
       double fontScale = 1.0;
       Point textOrg(topLeft.x + 10, topLeft.y + 50);
-      putText(frame, text, textOrg, FONT_HERSHEY_SIMPLEX, fontScale, Scalar(255, 0, 0), 2);
+      putText(frame, to_string(count), textOrg, FONT_HERSHEY_SIMPLEX, fontScale, Scalar(0, 0, 255), 2);
+      // std::cout << "Bits: " << std::bitset<16>(spots[i]) << std::endl;
+      if (spots[i] == 0xFFFF && flags[i] != BUSY) {
+        // all bits are on! car detected!
+        flags[i] = BUSY;
+        // std::cout << "spot[" << i << "] = " << std::bitset<16>(spots[i]) << " busy." << std::endl;
+        for (int s = 0; s < MANY_SPOTS; ++s) {
+          std::cout << " [" << (spots[s] == 0 ? "   " : "CAR" ) << "] ";
+        }
+        std::cout << std::endl;
+      }
+      if (spots[i] == 0 && flags[i] != OPEN) {
+        // isOpenSpot[i] = true;
+        flags[i] = OPEN;
+        // std::cout << "spot[" << i << "] = " << std::bitset<16>(spots[i]) << " open." << std::endl;
+        for (int s = 0; s < MANY_SPOTS; ++s) {
+          std::cout << " [" << (spots[s] == 0 ? "   " : "CAR" ) << "] ";
+        }
+        std::cout << std::endl;
+      }
+      // putText(frame, "<car|nocar>", textOrg, FONT_HERSHEY_SIMPLEX, fontScale, Scalar(255, 0, 0), 2);
+      bitPosition = (bitPosition + 1) % 16;
     }
 
     // show the result
-    imshow("gray", gray);
-    imshow("blurred", blurred);
-    imshow("edges", edges);
+    // imshow("gray", gray);
+    // imshow("blurred", blurred);
+    // imshow("edges", edges);
     imshow("main", frame);
 
     // break the loop on 'q' key press
-    if (waitKey(1) == 'q') {
+    if (waitKey(500) == 'q') {
         break;
     }
   }
@@ -116,82 +136,3 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
-
-/*
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <unistd.h>
-
-using namespace cv;
-using namespace std;
-
-int main(int argc, char** argv) {
-  std::string videoPath = "../tmp/parking/v4.mp4";
-  // string videoPath = "../tmp/parking/v1.mp4";
-  VideoCapture cap(videoPath);
-  if (!cap.isOpened()) {
-    cerr << "failed to open video" << endl;
-    return -1;
-  }
-
-  Mat frame, imgGray, imgBlur, imgThreshold;
-
-  vector<pair<Point, Point>> rectangles = {
-    { Point(25, 20), Point(280, 470) }
-    , { Point(305, 20), Point(590, 470) }
-    , { Point(600, 20), Point(850, 470) }
-  };
-
-  while (true) {
-    // capture a new frame
-    cap >> frame;
-    if (frame.empty()) {
-      cout << "end of video, looping..." << endl;
-      cap.set(CAP_PROP_POS_FRAMES, 0);
-      continue;
-    }
-
-    Scalar green(0, 255, 0), white(255, 255, 255);
-    int thickness = 2;
-
-    for (const auto& rect : rectangles) {
-      // convert frame to grayscale and apply glaussian blur
-      cvtColor(frame, imgGray, COLOR_BGR2GRAY);
-      GaussianBlur(imgGray, imgBlur, Size(3, 3), 1);
-      adaptiveThreshold(imgBlur, imgThreshold, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 25, 16);
-
-      // count non-zero pixels in the thresholded image
-      int count = countNonZero(imgThreshold);
-
-      // draw rectangles
-      rectangle(frame, rect.first, rect.second, green, thickness);
-      rectangle(imgThreshold, rect.first, rect.second, white, thickness);
-
-      // display the count on the frame
-      string text = to_string(count);
-      double fontScale = 1.0;
-      Point textOrg(rect.first.x + 10, rect.first.y + 50);
-      putText(frame, text, textOrg, FONT_HERSHEY_SIMPLEX, fontScale, green, thickness);
-      putText(imgThreshold, text, textOrg, FONT_HERSHEY_SIMPLEX, fontScale, white, thickness);
-    }
-
-    // display frames
-    imshow("video", frame);
-    imshow("imgThreshold", imgThreshold);
-
-    // break while loop on `q` key press
-    if (waitKey(1) == 'q') {
-      break;
-    }
-  }
-
-  // release the video capture object
-  cap.release();
-
-  // close all OpenCV windows
-  destroyAllWindows();
-
-  return 0;
-}
-*/
